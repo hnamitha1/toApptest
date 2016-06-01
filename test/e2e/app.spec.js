728416eefdcdfa48c8ff65258fca0d10907c7a5c
hnamitha1 <namitha.h@gmail.com>
2016-06-01 21:30:36 +0100

    11

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,7 +1,25 @@
 describe('Todos tracker', function() {
-  it('has a todo', function() {
+  it('has several ToDos', function() {
+    browser().get('/');
+    var todos = $$('#todos p');
+    expect(todos.first().getText()).toEqual('ToDo1: completed');
+    expect(todos.last().getText()).toEqual('ToDo2: not completed');
+  });
+
+  it('can add a ToDo', function() {
     browser.get('/');
-    var todo = $('#todo');
-    expect(todo.getText()).toEqual('ToDo1');
+    $('#new-todo-name').sendKeys("NewToDo");
+    $('#add-todo').click();
+
+    var todo = $$('#todos p').last().getText();
+    expect(todo).toEqual('NewToDo: not completed');
+  });
+
+  it('can remove a ToDo', function() {
+    browser.get('/');
+    var todos = $$('#todos p');
+
+    $('#remove-todo').click();
+    expect(todos.count()).toEqual(1);
   });
 });
