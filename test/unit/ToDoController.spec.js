b488670dd21b8cb15294ae4b6d8b192799587d9f
hnamitha1 <namitha.h@gmail.com>
2016-06-02 17:08:38 +0100

    services completed

--- test/unit/ToDoController.spec.js
+++ test/unit/ToDoController.spec.js
@@ -1,32 +1,36 @@
 describe('ToDoController', function() {
-  beforeEach(module('toDoApp'));
+  beforeEach(module('toDoApp'))
 
-  var ctrl, ToDoFactory;
+  var ctrl, httpBackend, ToDoFactory
+  var toDoData = [{text: "ToDo1", completed: true}, {text: "ToDo2", completed: false}]
 
-  beforeEach(inject(function($controller, _ToDoFactory_) {
-    ctrl = $controller('ToDoController');
-    ToDoFactory = _ToDoFactory_;
-  }));
+  beforeEach(inject(function($httpBackend, $controller, _ToDoFactory_) {
+    ctrl = $controller('ToDoController')
+    ToDoFactory = _ToDoFactory_
+    httpBackend = $httpBackend
+
+    httpBackend.expectGET("http://quiet-beach-24792.herokuapp.com/todos.json").respond(toDoData)
+    httpBackend.flush()
+  }))
 
   it('initialises with several todos', function() {
-    var todo1 = new ToDoFactory("ToDo1", true);
-    var todo2 = new ToDoFactory("ToDo2", false);
+    var todo1 = new ToDoFactory("ToDo1", true)
+    var todo2 = new ToDoFactory("ToDo2", false)
 
-    expect(ctrl.todos).toEqual([todo1, todo2]);
-  });
+    expect(ctrl.todos).toEqual([todo1, todo2])
+  })
 
   it('adds a new todo', function() {
-    ctrl.addToDo('NewToDo');
-
-    var todo = new ToDoFactory("NewToDo");
-    expect(ctrl.todos.pop()).toEqual(todo);
-  });
+    ctrl.addToDo('NewToDo')
+    var todo = new ToDoFactory("NewToDo")
+    expect(ctrl.todos.pop()).toEqual(todo)
+  })
 
   it('removes the last todo', function() {
-    initialCount = ctrl.todos.length;
+    initialCount = ctrl.todos.length
 
-    ctrl.removeToDo();
+    ctrl.removeToDo()
 
-    expect(ctrl.todos.length).toEqual(initialCount - 1);
-  });
-});
+    expect(ctrl.todos.length).toEqual(initialCount - 1)
+  })
+})
